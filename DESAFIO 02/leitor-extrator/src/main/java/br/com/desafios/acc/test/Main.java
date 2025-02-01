d8a195816f87a59f9ea712bf55ed7b89caf08106
barbourdev <felipe.barbourf@gmail.com>
2025-02-01 10:47:39 -0300

    Arquivo main para executar o projeto

--- DESAFIO 02/leitor-extrator/src/main/java/br/com/desafios/acc/test/Main.java
+++ DESAFIO 02/leitor-extrator/src/main/java/br/com/desafios/acc/test/Main.java
@@ -1,8 +1,22 @@
 package br.com.desafios.acc.test;
 
+import br.com.desafios.acc.service.ExportFile;
+import br.com.desafios.acc.service.ImportFile;
+
 public class Main {
 
     public static void main(String[] args){
-        System.out.println("Começando o desafio 02 ACC");
+        ImportFile importar = new ImportFile();
+        ExportFile exportar = new ExportFile();
+
+        String csvFile = "resources/lista.csv";
+        String txtFile = "resources/saida.txt";
+
+        //importar CSV para o banco
+        importar.importarFile(csvFile);
+
+        //exportar dados para TXT
+        exportar.exportarParaTXT(txtFile);
     }
 }
+
